07dff290fc78df88fa47e27f2591feb3b2ecd334
TheOpenDevProject <tkgs28@gmail.com>
2015-12-20 21:13:42 +1100

    Building

--- BuckyDaemon/target/debug/BuckyDaemon.d
+++
@@ -1,6 +0,0 @@
-/media/reverseinverse/Data/BuckyDaemon/BuckyDaemon/target/debug/BuckyDaemon: src/main.rs src/job_listener/mod.rs
-
-/media/reverseinverse/Data/BuckyDaemon/BuckyDaemon/target/debug/BuckyDaemon.d: src/main.rs src/job_listener/mod.rs
-
-src/main.rs:
-src/job_listener/mod.rs:
