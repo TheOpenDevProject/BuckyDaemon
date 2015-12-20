ee73d833961b7f9a8d0a312824c59ca1e4fbc431
TheOpenDevProject <tkgs28@gmail.com>
2015-12-20 21:55:32 +1100

    HTTP Listener Working

--- BuckyDaemon/src/main.rs
+++ BuckyDaemon/src/main.rs
@@ -1,6 +1,7 @@
+
 mod job_listener;
 use job_listener::JobListener;
 fn main(){
-	let test = JobListener::new("0.0.0.0".to_string(),9999);
-	test.output();
+	let test = JobListener::new("0.0.0.0:8999".to_string());
+	test.start("0.0.0.0:8999");
 }
