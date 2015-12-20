4ee6ccd93d86775e22022f8151c8dc832d0815e7
TheOpenDevProject <tkgs28@gmail.com>
2015-12-20 22:17:16 +1100

    Listening for request type. API will be RESTfull

--- BuckyDaemon/src/job_listener/mod.rs
+++ BuckyDaemon/src/job_listener/mod.rs
@@ -12,16 +12,23 @@ impl JobListener{
 	}
 
 	pub fn start(&self,server: &str ){
-	let server = Server::http(server).unwrap();
+		let server = Server::http(server).unwrap();
 
-	for request in server.incoming_requests() {
-    println!("received request! method: {:?}, url: {:?}",
-        request.method(),
-        request.url()
-    );
+		for request in server.incoming_requests() {
 
-    let response = Response::from_string("hello world");
-    request.respond(response);
+			match request.method(){
+				&tiny_http::Method::Get => self.handle_post(request.url()),
+				&tiny_http::Method::Post => self.handle_get(request.url()),
+				_ => println!("Unknown Method::Cant_Resolve")
+			}
+		}
 	}
-}
+
+	fn handle_post(&self, url: &str){
+		println!("POST request made {:?}",url);
+	}
+
+	fn handle_get(&self,url: &str){
+		println!("GET request made", );
+	}
 }
